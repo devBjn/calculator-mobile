96c91fc00275d227a5e871653214f2828ba57ba3
tambintv123 <tamnguyen.10092000tv@gmail.com>
2023-10-07 23:17:47 +0700

    handle error when click button C

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,19 +3,22 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
+import android.text.TextWatcher;
+import android.text.Editable;
 import com.google.android.material.button.MaterialButton;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.Undefined;
 
 public class MainActivity extends AppCompatActivity {
     TextView resultTv,solutionTv;
     MaterialButton buttonC,buttonBracketOpen,buttonBracketClose;
     MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
     MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
-    MaterialButton buttonAC,buttonDot;
+    MaterialButton buttonAC ,buttonDot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +27,12 @@ public class MainActivity extends AppCompatActivity {
         resultTv = findViewById(R.id.result_tv);
         solutionTv = findViewById(R.id.solution_tv);
 
-        assignId(buttonC, R.id.button_c);
+        // Set up TextWatchers for solutionTv and resultTv
+        solutionTv.addTextChangedListener(textWatcher);
+        resultTv.addTextChangedListener(textWatcher);
+
+        buttonC = findViewById(R.id.button_c);
+        buttonC.setOnClickListener(this::onClick);
         assignId(buttonBracketOpen,R.id.button_open_bracket);
         assignId(buttonBracketClose,R.id.button_close_bracket);
         assignId(buttonDivide,R.id.button_divide);
@@ -44,7 +52,29 @@ public class MainActivity extends AppCompatActivity {
         assignId(button9,R.id.button_9);
         assignId(buttonAC,R.id.button_ac);
         assignId(buttonDot,R.id.button_dot);
+
+        updateButtonCState();
     }
+    private void updateButtonCState() {
+        if (solutionTv.getText().length() < 1 || resultTv.getText().length() < 1) {
+            buttonC.setEnabled(false);
+        } else {
+            buttonC.setEnabled(true);
+        }
+    }
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            updateButtonCState();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {}
+    };
+
     void assignId(MaterialButton btn, int id){
         btn = findViewById(id);
         btn.setOnClickListener(this::onClick);
@@ -66,15 +96,9 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-//        if(solutionTv.getText().length() < 2 || resultTv.getText().length() < 2){
-//            buttonC.setEnabled(false);
-//        } else {
-//            buttonC.setEnabled(true);
-//        }
-
         if(buttonText.equals("C")){
             dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
-        }else{
+        } else {
             dataToCalculate = dataToCalculate+buttonText;
         }
 
@@ -91,6 +115,12 @@ public class MainActivity extends AppCompatActivity {
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initSafeStandardObjects();
             String finalResult = context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            Object evaluationResult = context.evaluateString(scriptable, data, "Javascript", 1, null);
+
+            if (evaluationResult == Undefined.instance) {
+               finalResult = "0";
+            }
+
             if(finalResult.endsWith(".0")){
                 finalResult = finalResult.replace(".0", "");
             }
